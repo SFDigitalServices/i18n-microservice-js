d43ae381b932710e61fca9bb9ac03dbd716cfaeb
Shawn Allen <shawnbot@users.noreply.github.com>
2020-04-15 21:55:02 -0700

    switch up where google credentials JSON gets parsed

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const { join } = require('path')
+const { readFileSync } = require('fs')
 const express = require('express')
 const google = require('./lib/google')
 
@@ -8,14 +9,14 @@ const {
   HOST = '0.0.0.0',
   PORT = '8001',
   GOOGLE_AUTH_CREDS_PATH = '.google-service-credentials.json',
-  GOOGLE_AUTH_CREDS = require(join(__dirname, GOOGLE_AUTH_CREDS_PATH))
+  GOOGLE_AUTH_CREDS = readFileSync(join(__dirname, GOOGLE_AUTH_CREDS_PATH), 'utf8')
 } = process.env
 
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.use('/google', google({ credentials: GOOGLE_AUTH_CREDS }))
+app.use('/google', google({ credentials: JSON.parse(GOOGLE_AUTH_CREDS) }))
 
 app.listen(PORT, () => {
   console.log(`Translation microservice running on ${HOST}:${PORT}`)
